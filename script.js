0144fb370f96663a5a257ad24007e5f2af0394fc
Jonathan Robles <roblesjonathan96@gmail.com>
2022-10-20 09:27:28 -0600

    changed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var $body = $('body');
 var $textInput = $('#textInput');
 var $keyInput = $('#keyInput');
 var $encryptButton = $('#encrypt');
+var $decryptButton = $('#decrypt');
 var $generateKey = $('#key');
 
 
@@ -13,9 +14,11 @@ run();
 
 
 function run(){
+    $decryptButton.click(function(){
+        decryptData($textInput.val(),$keyInput.val());
+    })
     $encryptButton.click(function(){
-        $textInput.val('');
-        encryptData($textInput.val(),$keyInput.val())
+        encryptData($textInput.val(),$keyInput.val());
     })
     $generateKey.click(function(){
         $keyInput.val('');
@@ -44,11 +47,32 @@ async function encryptData(message, secret) {
         console.error(error);
     }
 }
+async function decryptData(message, secret) {
+    try {
+        const url = 'https://classify-web.herokuapp.com/api/decrypt';
+        const jsonData = JSON.stringify({
+            data: message, key: secret
+        });
+        console.log(jsonData)
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: jsonData
+        });
+        const result = await response.json();
+        console.log(result);
+        $textInput.val(result.result);
+    } catch (error) {
+        console.error(error);
+    }
+}
 async function genKey(){
     try {
         const response = await fetch('https://classify-web.herokuapp.com/api/keygen?length=32?symbols=1');
-        $keyInput.val(response);
-        console.log(response.body)
+        const randKey = await response.json();
+        $keyInput.val(randKey.key);
     } catch (error) {
         console.error(error)
     }
